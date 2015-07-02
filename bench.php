99b7b6dbe9ef2706c8254244e34d87da325681e8
sergey <sergey@rusoft.ru>
2015-07-02 14:05:55 +0300

    Обновил тесты

--- bench.php
+++ bench.php
@@ -10,12 +10,19 @@
 #  Company     : Code24 BV, The Netherlands                              #
 #  Company     : Rusoft Ltd, Russia                                      #
 #  Date        : July 1, 2015                                            #
-#  version     : 1.01                                                    #
+#  version     : 1.0.2                                                   #
 #  License     : Creative Commons CC-BY license                          #
 #  Website     : http://www.php-benchmark-script.com                     #
 #                                                                        #
 ##########################################################################
 */
+
+$scriptVersion = '1.0.2';
+
+$stringTest = "    the quick <b>brown</b> fox jumps <i>over</i> the lazy dog and eat <span>lorem ipsum valar morgulis  \n\rабыр\nвалар дохаэрис         ";
+
+// Need alot of memory - more 1Gb
+$doTestArrays = false;
 
 function get_microtime()
 {
@@ -33,7 +40,6 @@ function convert($size)
     return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
 }
 
-
 	function test_Math($count = 1400000) {
 		$time_start = get_microtime();
 		$mathFunctions = array("abs", "acos", "asin", "atan", "decbin", "dechex", "decoct", "floor", "exp", "log1p", "sin", "tan", "pi", "is_finite", "is_nan", "sqrt", "rad2deg");
@@ -48,21 +54,149 @@ function convert($size)
 		return number_format(get_microtime() - $time_start, 3);
 	}
 
-	function test_StringManipulation($count = 1300000) {
+	function test_String_Simple($count = 1300000) {
+		global $stringTest;
 		$time_start = get_microtime();
-		$stringFunctions = array("addslashes", "chunk_split", "metaphone", "strip_tags", "crc32", "md5", "sha1", "strtoupper", "strtolower", "strrev", "strlen", "str_rot13", "soundex", "ord", "wordwrap");
+		$stringFunctions = array("strtoupper", "strtolower", "strrev", "strlen", "str_rot13", "ord", "trim");
 		foreach ($stringFunctions as $key => $function) {
 			if (!function_exists($function)) unset($stringFunctions[$key]);
 		}
-		$string = "the quick <b>brown</b> fox jumps <i>over</i> the lazy dog and eat <span>lorem ipsum volar morgulis";
 		for ($i=0; $i < $count; $i++) {
 			foreach ($stringFunctions as $function) {
-				$r = call_user_func_array($function, array($string));
+				$r = call_user_func_array($function, array($stringTest));
 			}
 		}
 		return number_format(get_microtime() - $time_start, 3);
 	}
 
+	function test_String_Multibyte($count = 130000) {
+		global $stringTest;
+		$time_start = get_microtime();
+		$stringFunctions = array("mb_strtoupper", "mb_strtolower", "mb_strlen", "mb_strwidth");
+		foreach ($stringFunctions as $key => $function) {
+			if (!function_exists($function)) unset($stringFunctions[$key]);
+		}
+		for ($i=0; $i < $count; $i++) {
+			foreach ($stringFunctions as $function) {
+				$r = call_user_func_array($function, array($stringTest));
+			}
+		}
+		return number_format(get_microtime() - $time_start, 3);
+	}
+
+	function test_String_Manipulation($count = 1300000) {
+		global $stringTest;
+		$time_start = get_microtime();
+		$stringFunctions = array("addslashes", "chunk_split", "metaphone", "strip_tags", "soundex", "wordwrap");
+		foreach ($stringFunctions as $key => $function) {
+			if (!function_exists($function)) unset($stringFunctions[$key]);
+		}
+		for ($i=0; $i < $count; $i++) {
+			foreach ($stringFunctions as $function) {
+				$r = call_user_func_array($function, array($stringTest));
+			}
+		}
+		return number_format(get_microtime() - $time_start, 3);
+	}
+
+	function test_Hashing($count = 1300000) {
+		global $stringTest;
+		$time_start = get_microtime();
+		$stringFunctions = array("crc32", "md5", "sha1",);
+		foreach ($stringFunctions as $key => $function) {
+			if (!function_exists($function)) unset($stringFunctions[$key]);
+		}
+		for ($i=0; $i < $count; $i++) {
+			foreach ($stringFunctions as $function) {
+				$r = call_user_func_array($function, array($stringTest));
+			}
+		}
+		return number_format(get_microtime() - $time_start, 3);
+	}
+
+	function test_Json_Encode($count = 1300000) {
+		global $stringTest;
+		$time_start = get_microtime();
+		$stringFunctions = array("json_encode",);
+		foreach ($stringFunctions as $key => $function) {
+			if (!function_exists($function)) unset($stringFunctions[$key]);
+		}
+		$data = array(
+			$stringTest,
+			123456,
+			123.456,
+			array(123456),
+			null,
+			false,
+		);
+		foreach ($stringFunctions as $function) {
+			for ($i=0; $i < $count; $i++) {
+				foreach ($data as $value) {
+					$r = call_user_func_array($function, array($value));
+				}
+			}
+		}
+		return number_format(get_microtime() - $time_start, 3);
+	}
+
+	function test_Json_Decode($count = 1300000) {
+		global $stringTest;
+		$time_start = get_microtime();
+		$stringFunctions = array("json_decode",);
+		foreach ($stringFunctions as $key => $function) {
+			if (!function_exists($function)) unset($stringFunctions[$key]);
+		}
+		$data = array(
+			$stringTest,
+			123456,
+			123.456,
+			array(123456),
+			null,
+			false,
+		);
+		foreach ($data as $key => $value) {
+			$data[ $key ] = json_encode($value);
+		}
+		foreach ($stringFunctions as $function) {
+			for ($i=0; $i < $count; $i++) {
+				foreach ($data as $value) {
+					$r = call_user_func_array($function, array($value));
+				}
+			}
+		}
+		return number_format(get_microtime() - $time_start, 3);
+	}
+
+	function test_Array_Fill($count = 3000) {
+		global $doTestArrays;
+		if (!$doTestArrays) return '-.---';
+
+		$time_start = get_microtime();
+		for($i = 0; $i < $count; ++$i) {
+			for($j = 0; $j < $count; ++$j) {
+				$X[ $i ][ $j ] = $i * $j;
+			}
+		}
+		return number_format(get_microtime() - $time_start, 3);
+	}
+
+	function test_Array_Unset($count = 3000) {
+		global $doTestArrays;
+		if (!$doTestArrays) return '-.---';
+
+		$time_start = get_microtime();
+		$X = range(0, $count);
+		for($i = 0; $i < $count; ++$i) {
+			$X[ $i ] = range(0, $count);
+		}
+		for($i = $count-1; $i >= 0; $i--) {
+			for($j = 0; $j < $count; ++$j) {
+				unset($X[ $i ][ $j ]);
+			}
+			unset($X[ $i ]);
+		}
+		return number_format(get_microtime() - $time_start, 3);
+	}
 
 	function test_Loops($count = 190000000) {
 		$time_start = get_microtime();
@@ -71,7 +205,6 @@ function convert($size)
 		return number_format(get_microtime() - $time_start, 3);
 	}
 
-	
 	function test_IfElse($count = 90000000) {
 		$time_start = get_microtime();
 		for ($i=0; $i < $count; $i++) {
@@ -79,16 +212,35 @@ function convert($size)
 			} elseif ($i == -2) {
 			} else if ($i == -3) {
 			} else {
-            }
+			}
 		}
 		return number_format(get_microtime() - $time_start, 3);
 	}
-	
-	
+
+	function test_Ternary($count = 90000000) {
+		$time_start = get_microtime();
+		for ($i=0; $i < $count; $i++) {
+			$r = ($i % 2 == 1)
+				? ( ($i % 3 == 1)
+					? ( ($i % 5 == 1)
+						? 3
+						: 2 )
+					: 1 )
+				: 0;
+		}
+		return number_format(get_microtime() - $time_start, 3);
+	}
+
 	$total = 0;
 	$functions = get_defined_functions();
 	$line = str_pad("-",38,"-");
-	echo "<pre>\n$line\n|".str_pad("PHP BENCHMARK SCRIPT",36," ",STR_PAD_BOTH)."|\n$line\nStart : ".date("Y-m-d H:i:s")."\nServer : ".php_uname()."\nPHP version : ".PHP_VERSION."\nPlatform : ".PHP_OS. "\n$line\n";
+	echo "<pre>\n$line\n|".str_pad("PHP BENCHMARK SCRIPT",36," ",STR_PAD_BOTH)
+		."|\n$line\nStart : ".date("Y-m-d H:i:s")
+		."\nServer : ".php_uname()
+		."\nPHP version : ".PHP_VERSION
+		."\nBenchmark version : ".$scriptVersion
+		."\nPlatform : ".PHP_OS
+		. "\n$line\n";
 	foreach ($functions['user'] as $user) {
 		if (preg_match('/^test_/', $user)) {
 			$result = $user();
