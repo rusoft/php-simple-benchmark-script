1a6758fc6e2ccfb15dc12a60655eda7e35d18d2c
Сергей Дрябжинский <sergey@rusoft.ru>
2025-03-20 15:27:54 +0300

    Загрузить файлы в «/»

--- php-options.php
+++ php-options.php
@@ -15,4 +15,15 @@ ini_set("xpcache.cacher", 0);
 ini_set("apc.enable", 0);
 
 ini_set("xdebug.default_enable", 0);
+ini_set("xdebug.show_exception_trace", 0);
+ini_set("xdebug.remote_autostart", 0);
+ini_set("xdebug.remote_enable", 0);
 ini_set("xdebug.mode", "off");
+
+ini_set("output_buffering", 0);
+ini_set("max_execution_time", 600);
+ini_set("memory_limit", "130M");
+ini_set("mbstring.internal_encoding", "UTF-8");
+ini_set("mbstring.func_overload", 0);
+
+ini_set("date.timezone", "Europe/Moscow");
